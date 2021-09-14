aa1d0db9af8a117567c9e1c5ec3ffc5fb1c64957
azeunkn0wn <azeunkn0wn@gmail.com>
2021-09-14 21:28:50 +0800

    scrollable about page

--- lib/page/about/aboutpage.dart
+++ lib/page/about/aboutpage.dart
@@ -10,50 +10,53 @@ class AboutPage extends StatelessWidget {
             elevation: 0,
             title: Text('About'),
           ),
-          body: Column(
-            children: [
-              Container(
-                color: Theme.of(context).primaryColor,
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  child: PhotoHero(
-                    photo: 'assets/images/Panay Island Translator (round).png',
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(20),
-                child: RichText(
-                  textScaleFactor: 1.3,
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    // Note: Styles for TextSpans must be explicitly defined.
-                    // Child text spans will inherit styles from parent
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      color: Colors.black,
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  color: Theme.of(context).primaryColor,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: PhotoHero(
+                      photo:
+                          'assets/images/Panay Island Translator (round).png',
                     ),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text:
-                              'The Panay Island Language Translator was developed by '),
-                      TextSpan(
-                          text: 'Glenn D. Dionio, ',
-                          style: new TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(
-                          text: 'Carol Jean T. Arela, ',
-                          style: new TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(
-                          text: 'Anne Margarette Clavaton ',
-                          style: new TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(
-                          text:
-                              '(2021-2022) students of Filamer Christian University Roxas City. The app named Panay Island Language Translator. This application gives us the flexibility to access our system from nowhere through a powerful mobile app.'),
-                    ],
                   ),
                 ),
-              ),
-            ],
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: RichText(
+                    textScaleFactor: 1.3,
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      // Note: Styles for TextSpans must be explicitly defined.
+                      // Child text spans will inherit styles from parent
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text:
+                                'The Panay Island Language Translator was developed by '),
+                        TextSpan(
+                            text: 'Glenn D. Dionio, ',
+                            style: new TextStyle(fontWeight: FontWeight.bold)),
+                        TextSpan(
+                            text: 'Carol Jean T. Arela, ',
+                            style: new TextStyle(fontWeight: FontWeight.bold)),
+                        TextSpan(
+                            text: 'Anne Margarette Clavaton ',
+                            style: new TextStyle(fontWeight: FontWeight.bold)),
+                        TextSpan(
+                            text:
+                                '(2021-2022) students of Filamer Christian University Roxas City. The app named Panay Island Language Translator. This application gives us the flexibility to access our system from nowhere through a powerful mobile app.'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           )
           // body: CustomScrollView(
           //   slivers: [
